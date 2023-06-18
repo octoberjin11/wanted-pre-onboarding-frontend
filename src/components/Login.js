4433df80adcce7b3cbe77aad30d0b9a6f9858b42
heejoung <octoberjin11@gmail.com>
2023-06-18 21:38:10 +0900

    로그인 기능 완료

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,35 +1,109 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import React, {useState, useEffect} from 'react';
+import {Link, useNavigate} from 'react-router-dom';
 
 function Login() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [isValidEmail, setIsValidEmail] = useState(false);
+    const [isValidPassword, setIsValidPassword] = useState(false);
+    const [activeButton, setActiveButton] = useState(false);
+    const [loginError, setLoginError] = useState(false);
 
-    const onSubmitHandler = () => {
-        console.log('제출');
+    const navigate = useNavigate();
+
+    // 이메일 조건 검사: '@' 포함
+    const validEmail = () => {
+        const isValid = email.includes('@');
+        setIsValidEmail(isValid);
+    };
+    // 비밀번호 8자 이상
+    const validPassword = (e) => {
+        const isValid = password.length >= 8;
+        setIsValidPassword(isValid);
     };
 
-    const onChangeEmail = (e) => {
-        setEmail(e.currentTarget.value);
+    // 로그인
+    const onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        const payload = {
+            email: email,
+            password: password
+        };
+
+        fetch('https://www.pre-onboarding-selection-task.shop/auth/signin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        })
+            .then((res) => {
+                //console.log(res);
+                return res.json();
+            })
+            .then((res) => {
+                //console.log(res);
+
+                // 로그인 성공
+                if (res.access_token) {
+                    const accessToken = res.access_token;
+                    localStorage.setItem('auth', accessToken);
+                    navigate('/todo');
+                }
+
+                // 로그인 실패
+                if (res.statusCode === 401) {
+                    setLoginError(true);
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     };
 
-    const onChangePassword = (e) => {
-        setPassword(e.currentTarget.value);
-    };
+    // 이메일, 비밀번호 유효성 검사
+    useEffect(() => {
+        validEmail();
+        validPassword();
+        setLoginError(false);
+    }, [email, password]);
 
-    const onClickLogin = () => {};
+    // 버튼 활성화
+    useEffect(() => {
+        if (isValidEmail && isValidPassword) {
+            setActiveButton(true);
+        } else {
+            setActiveButton(false);
+        }
+    }, [isValidEmail, isValidPassword]);
 
     return (
         <div id="membership">
+            <h1>로그인</h1>
             <div className="memberBox">
                 <form onSubmit={onSubmitHandler}>
-                    <label>Email</label>
-                    <input data-testid="email-input" value={email} onChange={onChangeEmail} />
+                    <label>이메일</label>
+                    <input
+                        data-testid="email-input"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    {!isValidEmail && <p className="errorTxt">이메일 주소는 @를 포함하여 입력해주세요.</p>}
                     <br />
-                    <label>Password</label>
-                    <input type="password" data-testid="password-input" value={password} onChange={onChangePassword} />
+                    <label>비밀번호</label>
+                    <input
+                        type="password"
+                        data-testid="password-input"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
+                    {!isValidPassword && <p className="errorTxt">비밀번호는 8자 이상으로 입력해주세요.</p>}
+                    {loginError && <p className="errorTxt">이메일/비밀번호가 맞지 않습니다.</p>}
                     <br />
-                    <button data-testid="signin-button" className="btnPrimary" onClick={onClickLogin}>
+                    <button type="submit" data-testid="signin-button" className="btnPrimary" disabled={!activeButton}>
                         로그인
                     </button>
                 </form>
